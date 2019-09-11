90608950f7e550d11dc6dfcadef1612087279738
rai.maciel <rai.maciel@basis.com.br>
2019-09-11 15:55:01 -0300

    inserindo modulo de cadastro

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -7,10 +7,12 @@ import { AppComponent } from './app.component';
 
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { FormsModule } from '@angular/forms';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
