5530420d87c532dcf7ba3e5097523ec455a3712d
rai.maciel <rai.maciel@basis.com.br>
2019-10-03 09:34:02 -0300

    adicionando animacao na tela de login

--- front/src/app/principal/principal.component.ts
+++ front/src/app/principal/principal.component.ts
@@ -11,7 +11,16 @@ export class PrincipalComponent implements OnInit {
     {autor: 'Helmsmasher', imgCapa: '../../assets/imagemDeEquipe.jpg', comentarios: '5', titulo: 'Yordle Party!' },
     {autor: 'Arya', imgCapa: '../../assets/imagemDeEquipe2.jpg', comentarios: '1', titulo: 'Tudo pelo Yasuo.' },
     {autor: 'Faker', imgCapa: '../../assets/imagemDeEquipe3.jpg', comentarios: '24', titulo: 'SKT T1 Final 2016' },
+    {autor: 'Helmsmasher', imgCapa: '../../assets/imagemDeEquipe4.jpg', comentarios: '7', titulo: 'Only Girls' },
+    {autor: 'Helmsmasher', imgCapa: '../../assets/imagemDeEquipe4.jpg', comentarios: '7', titulo: 'Fnatic' },
     {autor: 'Helmsmasher', imgCapa: '../../assets/imagemDeEquipe4.jpg', comentarios: '7', titulo: 'Only Girls' }, 
+  ]
+
+  composicoesMaisVotadas: any[] = [
+    {autor: 'Helmsmasher', imgCapa: '../../assets/Jinx.png', comentarios: '5', titulo: 'Yordle Party!' },
+    {autor: 'Arya', imgCapa: '../../assets/Jinx.png', comentarios: '1', titulo: 'Tudo pelo Yasuo.' },
+    {autor: 'Faker', imgCapa: '../../assets/Ezreal.png', comentarios: '24', titulo: 'SKT T1 Final 2016' },
+    {autor: 'Helmsmasher', imgCapa: '../../assets/Lux.png', comentarios: '7', titulo: 'Only Girls' }, 
   ]
 
 
