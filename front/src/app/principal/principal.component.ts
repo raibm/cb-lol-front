435ac5f15b5d38774fcb2ecaad9a9071e08a676b
rai.maciel <rai.maciel@basis.com.br>
2019-10-04 15:09:18 -0300

    exibicao dos 5 campeoes

--- front/src/app/principal/principal.component.ts
+++ front/src/app/principal/principal.component.ts
@@ -23,6 +23,19 @@ export class PrincipalComponent implements OnInit {
     {autor: 'Helmsmasher', imgCapa: '../../assets/Lux.png', comentarios: '7', titulo: 'Only Girls' }, 
   ]
 
+  campeoes: any[] = [
+    {nome: 'Aatrox', imagem: '../../assets/opcoesCampeoes/Aatrox_Render.png', exibir: true},
+    {nome: 'Ahri', imagem: '../../assets/opcoesCampeoes/Ahri_Render.png', exibir: true},
+    {nome: 'Akali', imagem: '../../assets/opcoesCampeoes/Akali_Render.png', exibir: true},
+    {nome: 'Alistar', imagem: '../../assets/opcoesCampeoes/Alistar_Render.png', exibir: true},
+    {nome: 'Amumu', imagem: '../../assets/opcoesCampeoes/Amumu_Render.png'},
+    {nome: 'Anivia', imagem: '../../assets/opcoesCampeoes/Anivia_Render.png'},
+    {nome: 'Annie', imagem: '../../assets/opcoesCampeoes/Annie_Render.png'},
+    {nome: 'Ashe', imagem: '../../assets/opcoesCampeoes/Ashe_Render.png', exibir: true},
+    {nome: 'Aurelion Sol', imagem: '../../assets/opcoesCampeoes/Aurelion_Sol_Render.png'},
+    {nome: 'Azir', imagem: '../../assets/opcoesCampeoes/Azir_Render.png'},
+  ]
+
 
   constructor() { }
 
